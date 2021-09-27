1b198f3296748866756b8cffaaf4dc4eb6b55f5f
Nicolas Acosta <nicolas.acosta@live.co.uk>
2021-09-27 18:52:40 +0100

    now i know why i am getting the list error. it is because if i am in a middle of an idle animation before going into a walking animation, the animation doesn't reset and the value of currentFrame isn't reset before going into the new animation. also the currentanimation list doesnt change after the sprite goes into a walking animation, causing the error.

--- player.py
+++ player.py
@@ -30,7 +30,7 @@ class Player(pygame.sprite.Sprite):
         self.left_walk = None
         self.right_walk = None
 
-        self.moving = False
+        self.moving = 0
 
         self.direction = 1
         # self.standing = 1
@@ -38,7 +38,7 @@ class Player(pygame.sprite.Sprite):
         self.currentFrame = 0
         self.currentAnimation = []
         self.timeSinceFrame = 0
-        self.timeBetweenSteps = 1 / 5
+        self.timeBetweenSteps = 1 / 10
 
     def player_sprite(self):
         # idle spritesheets
@@ -78,45 +78,25 @@ class Player(pygame.sprite.Sprite):
     def draw(self, window_surface):
         # player_rect = pygame.Rect(self.player_pos_x, self.player_pos_y, 48, 48)
         # pygame.draw.rect(self.window_surface, self.colour, player_rect)
-        if self.direction == 0:  # and not self.w:
-            self.currentAnimation = self.up_idle
-        if self.direction == 0 and self.w:
-            self.currentAnimation = self.up_walk
+        if self.moving == 0:
+            if self.direction == 0:
+                self.currentAnimation = self.up_idle
+            if self.direction == 1:
+                self.currentAnimation = self.down_idle
+            if self.direction == 2:
+                self.currentAnimation = self.left_idle
+            if self.direction == 3:
+                self.currentAnimation = self.right_idle
 
-        if self.direction == 1:  # and not self.s:
-            self.currentAnimation = self.down_idle
-        if self.direction == 1 and self.s:
-            self.currentAnimation = self.down_walk
-
-        if self.direction == 2:  # and not self.a:
-            self.currentAnimation = self.left_idle
-        if self.direction == 2 and self.a:
-            self.currentAnimation = self.left_walk
-
-        if self.direction == 3:  # and not self.d:
-            self.currentAnimation = self.right_idle
-        if self.direction == 3 and self.d:
-            self.currentAnimation = self.right_walk
-
-        # if self.direction == 0 and self.standing == 1:
-        #     self.currentAnimation = self.up_idle
-        # elif self.direction == 0 and self.standing == 0:
-        #     self.currentAnimation = self.up_walk
-        #
-        # if self.direction == 1 and self.standing == 1:
-        #     self.currentAnimation = self.down_idle
-        # elif self.direction == 1 and self.standing == 0:
-        #     self.currentAnimation = self.down_walk
-        #
-        # if self.direction == 2 and self.standing == 1:
-        #     self.currentAnimation = self.left_idle
-        # elif self.direction == 2 and self.standing == 0:
-        #     self.currentAnimation = self.left_walk
-        #
-        # if self.direction == 3 and self.standing == 1:
-        #     self.currentAnimation = self.right_idle
-        # elif self.direction == 3 and self.standing == 0:
-        #     self.currentAnimation = self.right_walk
+        if self.moving == 1:
+            if self.direction == 0 and self.w:
+                self.currentAnimation = self.up_walk
+            if self.direction == 1 and self.s:
+                self.currentAnimation = self.down_walk
+            if self.direction == 2 and self.a:
+                self.currentAnimation = self.left_walk
+            if self.direction == 3 and self.d:
+                self.currentAnimation = self.right_walk
 
         frame = self.currentAnimation[self.currentFrame]
         window_surface.blit(frame, (self.player_pos_x, self.player_pos_y))
@@ -125,52 +105,60 @@ class Player(pygame.sprite.Sprite):
         self.currentFrame += 1
         if self.currentFrame >= len(self.currentAnimation):
             self.currentFrame = 0
+        # if self.currentAnimation != self.currentAnimation:
+        #     self.currentFrame //= 8
 
     def update(self, time_delta):
         self.timeSinceFrame += time_delta
         if self.timeSinceFrame >= self.timeBetweenSteps:
             self.timeSinceFrame = 0
             self.next_frame()
+
         if self.w:
+            # self.currentAnimation = self.up_walk
             self.player_pos_y -= self.speed * time_delta
         if self.s:
+            # self.currentAnimation = self.down_walk
             self.player_pos_y += self.speed * time_delta
         if self.d:
+            # self.currentAnimation = self.right_walk
             self.player_pos_x += self.speed * time_delta
         if self.a:
+            # self.currentAnimation = self.left_walk
             self.player_pos_x -= self.speed * time_delta
 
     def on_key_press(self, event):
         if event.type == pygame.KEYDOWN:
+            # self.moving = 1
             if event.key == pygame.K_w:
                 self.direction = 0
                 self.w = True
+                self.moving = 1
             if event.key == pygame.K_s:
                 self.direction = 1
                 self.s = True
+                self.moving = 1
             if event.key == pygame.K_a:
                 self.direction = 2
                 self.a = True
+                self.moving = 1
             if event.key == pygame.K_d:
                 self.direction = 3
                 self.d = True
+                self.moving = 1
 
     def on_key_release(self, event):
         if event.type == pygame.KEYUP:
-            # self.moving = False
+            # self.moving = 0
             if event.key == pygame.K_w:
                 self.w = False
-                # self.moving = False
-                # self.standing = 1
+                self.moving = 0
             if event.key == pygame.K_s:
                 self.s = False
-                # self.moving = False
-                # self.standing = 1
+                self.moving = 0
             if event.key == pygame.K_a:
                 self.a = False
-                # self.moving = False
-                # self.standing = 1
+                self.moving = 0
             if event.key == pygame.K_d:
                 self.d = False
-                # self.moving = False
-                # self.standing = 1
+                self.moving = 0
